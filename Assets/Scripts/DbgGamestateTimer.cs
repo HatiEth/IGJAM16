1e749aedae17456ddd42935798013f8116240710
Hati Eth <hati.eth@gmail.com>
2016-08-17 20:47:02 +0200

    added timer

---
+++ Assets/Scripts/DbgGamestateTimer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System.Collections;
+
+public class DbgGamestateTimer : MonoBehaviour {
+
+	private FaceGameState m_FaceGame;
+
+	// Use this for initialization
+	void Start () {
+		m_FaceGame = FindObjectOfType<FaceGameState>();
+	
+		m_FaceGame.m_pPartyTimerS.Subscribe((time) =>
+		{
+			GetComponent<Text>().text = "" + time;
+		}).AddTo(this.gameObject);
+	}
+	
+}
--- Assets/Scripts/FaceGameState.cs
+++ Assets/Scripts/FaceGameState.cs
@@ -7,15 +7,38 @@ public class FaceGameState : MonoBehaviour {
 
 	private CompositeDisposable _disposeables = new CompositeDisposable();
 
+	[HideInInspector]
 	public int PersonExpectedExpression;
+	[HideInInspector]
 	public int YourCurrentExpression;
+
+	[Tooltip("Total time of party left in seconds")]
+	/* 
+	 * Total time of party left in seconds 
+	 */
+	public float PartyTimeSeconds;
+
+	public ReactiveProperty<float> m_pPartyTimerS;
+	public float PartyTimerS
+	{
+		get { return m_pPartyTimerS.Value; }
+		set { m_pPartyTimerS.Value = value; }
+	}
 
 	public Subject<bool> m_MatchedExpression;
 
+	private void Awake()
+	{
+		m_pPartyTimerS = new ReactiveProperty<float>(PartyTimeSeconds);
+		m_MatchedExpression = new Subject<bool>();
+	}
+
+
 	private void Start()
 	{
+		
 		GenerateExpectedExpression();
-		m_MatchedExpression = new Subject<bool>();
+		
 
 		m_MatchedExpression.Subscribe((gotItRight) =>
 		{
@@ -43,5 +66,10 @@ public class FaceGameState : MonoBehaviour {
 	public void GenerateExpectedExpression()
 	{
 		PersonExpectedExpression = Random.Range(0, 7);
+	}
+
+	void Update()
+	{
+		PartyTimerS -= Time.deltaTime;
 	}
 }
