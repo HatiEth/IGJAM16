74319a512dced5b113307ccb439e452ff01783b0
Hati Eth <hati.eth@gmail.com>
2016-08-18 15:22:05 +0200

    girlfriend

--- Assets/Scripts/Girlfriend.cs
+++ Assets/Scripts/Girlfriend.cs
@@ -7,19 +7,35 @@ using System.Collections.Generic;
 
 public class Girlfriend : MonoBehaviour {
 
+	public Image GirlfriendFacialSpriteRenderer;
 	public Sprite[] FacialExpressions = new Sprite[System.Enum.GetNames(typeof(Faces)).Length];
+
+	public Image[] Togglables;
 
 	void Start()
 	{
-		Text textBox = GetComponent<Text>();
+		foreach(var toggle in Togglables)
+		{
+			toggle.enabled = false;
+		}
 
 		MessageBroker.Default.Receive<PersonReady>().Subscribe(msg =>
 		{
-			if(!msg.Person.HasMet)
+			if (!msg.Person.HasMet)
 			{
-				textBox.text = "be " + msg.Person.RequiredFaceExpression;
-
-				// GetComponent<SpriteRenderer>().sprite = FacialExpressions[(int)msg.Person.RequiredFaceExpression];
+				foreach (var toggle in Togglables)
+				{
+					toggle.enabled = true;
+				}
+				GirlfriendFacialSpriteRenderer.sprite = FacialExpressions[(int)msg.Person.RequiredFaceExpression];
+				
+			}
+			else
+			{
+				foreach (var toggle in Togglables)
+				{
+					toggle.enabled = false;
+				}
 			}
 
 		}).AddTo(this.gameObject);
