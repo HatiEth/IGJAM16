361a677f620f2518248caaba9d67702557846b18
Priareos <priareos@tawsm.com>
2016-08-18 12:45:38 +0200

    FIXED: missing time CHANGED: sorting

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 using System.Collections;
 
-public class Clock : MonoBehaviour {
-
+public class Clock : MonoBehaviour
+{
     public GameObject clockRotator;
     public GameObject clockFill;
+    public GameObject clockTimer;
+    private FaceGameState m_FaceGame;
 
-	public void UpdateClock (float percentRemaining) {
-        clockFill.GetComponent<Image>().fillAmount = percentRemaining;
-        clockRotator.transform.eulerAngles = new Vector3(0, 0, percentRemaining * -360.0f);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        m_FaceGame = FindObjectOfType<FaceGameState>();
+
+        m_FaceGame.m_pPartyTimerS.Subscribe((time) =>
+        {
+            clockTimer.GetComponent<Text>().text = "" + time;
+            clockFill.GetComponent<Image>().fillAmount = (time / 180);
+            clockRotator.transform.eulerAngles = new Vector3(0, 0, (time / 180) * 360.0f);
+        }).AddTo(this.gameObject);
+    }
 }
