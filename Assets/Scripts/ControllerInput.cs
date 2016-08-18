12eb5a086223cf4c7d30c3f717b6053b45917692
Priareos <priareos@tawsm.com>
2016-08-18 23:23:37 +0200

    CHANGED: lovebar, girlfriend

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -9,25 +9,23 @@ public class ControllerInput : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Joystick1Button0))
         {
-            ExecuteEvents.Execute<IPointerEnterHandler>(buttons[0], new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+            ExecuteEvents.Execute<IPointerDownHandler>(buttons[0], new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
             ExecuteEvents.Execute<IPointerClickHandler>(buttons[0], new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
-            ExecuteEvents.Execute<IPointerExitHandler>(buttons[0], new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
-            Debug.Log("AButton");
         }
         else if (Input.GetKeyDown(KeyCode.JoystickButton1))
         {
+            ExecuteEvents.Execute<IPointerDownHandler>(buttons[1], new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
             ExecuteEvents.Execute<IPointerClickHandler>(buttons[1], new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
-            Debug.Log("BButton");
         }
         else if (Input.GetKeyDown(KeyCode.JoystickButton2))
         {
+            ExecuteEvents.Execute<IPointerDownHandler>(buttons[2], new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
             ExecuteEvents.Execute<IPointerClickHandler>(buttons[2], new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
-            Debug.Log("XButton");
         }
         else if (Input.GetKeyDown(KeyCode.JoystickButton3))
         {
+            ExecuteEvents.Execute<IPointerDownHandler>(buttons[3], new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
             ExecuteEvents.Execute<IPointerClickHandler>(buttons[3], new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
-            Debug.Log("YButton");
         }
         }
 }
