921a39237813aa603b2a021a8731903a436bc842
Priareos <priareos@tawsm.com>
2016-08-19 01:15:20 +0200

    ADDED: heart fx, gameoverscene + scoring, thoughtbubble

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public Text timeText;
+
+    public void UpdateScore(float score, float time)
+        {
+            scoreText.text = "" + score;
+            timeText.text = time + " seconds"; 
+        }
+}
