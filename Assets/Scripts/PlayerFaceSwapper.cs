1b319276014420e2c89cea4b89f4ff98150cda72
dienerM <michael.diener@stud.h-da.de>
2016-08-19 04:07:02 +0200

    Added player facial expressions

---
+++ Assets/Scripts/PlayerFaceSwapper.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerFaceSwapper : MonoBehaviour {
+
+    public List<Sprite> PlayerFaces = new List<Sprite>();
+    public SpriteRenderer FaceSpriteRenderer;
+
+    void Start()
+    {
+        MessageBroker.Default.Receive<PlayerChoosedExpression>().Subscribe(msg =>
+        {
+            FaceSpriteRenderer.sprite = PlayerFaces[(int)msg.FacialExpression];
+        }
+        
+        );
+    }
+}
---
+++ Assets/Scripts/Thoughtbubble.cs
@@ -0,0 +1,28 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Thoughtbubble : MonoBehaviour {
+
+    public string[] Thoughts;
+    public Text Thought_Text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        MessageBroker.Default.Receive<PlayerChoosedExpression>().Subscribe(msg =>
+        {
+            if (Random.Range(0,9) == 1 && Thoughts.Length > 0 )
+            {
+                
+                Thought_Text.text = Thoughts[Random.Range(0, Thoughts.Length - 1)];
+                GetComponent<Animator>().Play(0);
+            }
+           
+        }
+        
+        );
+    }
+}
